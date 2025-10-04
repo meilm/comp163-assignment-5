3de2eca01f6a9f8a93072eb463c41c391fe55561
itsmeilm <isaaclmanson@gmail.com>
2025-10-03 22:09:53 -0400

    Completed Challenge 3

--- ilmanson_assignment_5.py
+++ ilmanson_assignment_5.py
@@ -10,9 +10,9 @@ while n != 1:
         n *= 3
         n += 1
     step_count += 1
-if step_count == 0: # stupid shit i have to do to match the zybooks whitespace
+if step_count == 0:                         # stupid shit i have to do to match the zybooks whitespace
     print(int(n), 'Steps:', step_count) 
-else: # omit the newline before "Steps:" if the input is 1 because zybooks is stupid
+else:                                       # omit the newline before "Steps: " if the input is 1 because zybooks is stupid
     print(int(n))
     print('Steps:', step_count)
 
@@ -24,3 +24,25 @@ for divisor in range(2, n):
         print(f'{n} is not prime (divisible by {divisor})')
         break
 else: print(n, 'is prime!')
+
+print('\n=== Challenge 3: Multiplication Table ===')
+print('Multiplication Table:')
+row_values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print('   ', end='')
+for item in row_values:
+    if item < 10:                           # determine how many spaces to print to keep the table organized
+        print('  ', item, end='') 
+    else: print(' ', item, end='')
+print()
+for row in row_values:
+    if row < 10:                            # same thing here
+        print('', row, end=' ')
+    else: print(row, end=' ') 
+    for item in row_values: 
+        current_value = item * row
+        if current_value >= 100:            # and here, too
+            print('', current_value, end='') 
+        elif current_value >= 10:
+            print(' ', current_value, end='')
+        else: print('  ', current_value, end='') 
+    print()
