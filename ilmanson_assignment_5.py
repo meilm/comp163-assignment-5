cfdac6f529747c4f0a3ca6092164f8434004d3ce
itsmeilm <isaaclmanson@gmail.com>
2025-10-03 22:34:06 -0400

    Added detailed comments

--- ilmanson_assignment_5.py
+++ ilmanson_assignment_5.py
@@ -1,16 +1,16 @@
 print('=== Challenge 1: Collatz Conjecture ===')
 n = int(input('Enter starting number: '))
-print('Sequence: ', end='') 
+print('Sequence: ', end='')
 step_count = 0
-while n != 1:
+while n != 1:                               # while loop, loops until the number is equal to one
     print(int(n), end=' ')
-    if n % 2 == 0:
-        n /= 2
-    else:
-        n *= 3
-        n += 1
+    if n % 2 == 0:                          # if n is even
+        n /= 2                                  # divide by 2
+    else:                                   # if n is odd
+        n *= 3                              #   multiply n by 3   
+        n += 1                              #   and add 1 to n
     step_count += 1
-if step_count == 0:                         # stupid shit i have to do to match the zybooks whitespace
+if step_count == 0:                         # stupid stuff i have to do to match the zybooks whitespace
     print(int(n), 'Steps:', step_count) 
 else:                                       # omit the newline before "Steps: " if the input is 1 because zybooks is stupid
     print(int(n))
@@ -19,8 +19,8 @@ else:                                       # omit the newline before "Steps: " 
 print('\n=== Challenge 2: Prime Number Checker ===')
 n = int(input('Enter a number: '))
 print(f'Testing divisors from 2 to {n-1}...')
-for divisor in range(2, n):
-    if n % divisor == 0:
+for divisor in range(2, n):                  
+    if n % divisor == 0:                    # if n is divisibly by the current number
         print(f'{n} is not prime (divisible by {divisor})')
         break
 else: print(n, 'is prime!')
@@ -29,18 +29,18 @@ print('\n=== Challenge 3: Multiplication Table ===')
 print('Multiplication Table:')
 row_values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print('   ', end='')
-for item in row_values:
-    if item < 10:                           # determine how many spaces to print to keep the table organized
+for item in row_values:                     # runs once for each item in the list "row_values" (1 through 10)
+    if item < 10:                           #   determine how many spaces to print to keep the table organized
         print('  ', item, end='') 
     else: print(' ', item, end='')
 print()
-for row in row_values:
-    if row < 10:                            # same thing here
-        print('', row, end=' ')
+for row in row_values:                      # the list has numbers 1 through 10 in it, so i can use it to say which row we are on
+    if row < 10:                            #   determine how many spaces to print to keep the table organized x2
+        print('', row, end=' ')               
     else: print(row, end=' ') 
-    for item in row_values: 
+    for item in row_values:                 # for each item in the current row of the table
         current_value = item * row
-        if current_value >= 100:            # and here, too
+        if current_value >= 100:            # determine how many spaces to print to keep the table organized x3
             print('', current_value, end='') 
         elif current_value >= 10:
             print(' ', current_value, end='')
