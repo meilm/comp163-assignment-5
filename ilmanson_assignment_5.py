88a3ef8d6124b3babb6be173bb9041670ecb17c3
itsmeilm <isaaclmanson@gmail.com>
2025-10-03 20:52:06 -0400

    Completed challenge 2

--- ilmanson_assignment_5.py
+++ ilmanson_assignment_5.py
@@ -15,3 +15,12 @@ if step_count == 0: # stupid shit i have to do to match the zybooks whitespace
 else: # omit the newline before "Steps:" if the input is 1 because zybooks is stupid
     print(int(n))
     print('Steps:', step_count)
+
+print('\n=== Challenge 2: Prime Number Checker ===')
+n = int(input('Enter a number: '))
+print(f'Testing divisors from 2 to {n-1}...')
+for divisor in range(2, n):
+    if n % divisor == 0:
+        print(f'{n} is not prime (divisible by {divisor})')
+        break
+else: print(n, 'is prime!')
